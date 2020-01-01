9c89c9407008cf2a6d44bcfe4c4cfcb01b54cee8
Benjamin <bhoogter@yahoo.com>
2020-01-01 17:02:46 -0500

    Update dependency-manager.php

--- sample/dependency-manager.php
+++ sample/dependency-manager.php
@@ -2,4 +2,4 @@
 
 // Place the dependency manager phar in the same directory ()
 define('DEPENDENCY_MANAGER_PHAR', __DIR__ . "/php-dependency-manager.phar");
-require_once("phar://" . DEPENDENCY_MANAGER_PHAR . "/src/class-dependency-manager.phar");
+require_once("phar://" . DEPENDENCY_MANAGER_PHAR . "/src/class-dependency-manager.php");
