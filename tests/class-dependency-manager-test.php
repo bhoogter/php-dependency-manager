c8952dd3a61da70f7b6fad72e758c50c864cdec1
Ben <bhoogter@ford.com>
2020-01-01 16:29:09 -0500

    Version 1.0.0

--- tests/class-dependency-manager-test.php
+++ tests/class-dependency-manager-test.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-require(__DIR__ . "/../src/class-dependency-manager.php");
+require_once(__DIR__ . "/../src/class-dependency-manager.php");
 
 final class dependency_manager_test extends TestCase
 {
