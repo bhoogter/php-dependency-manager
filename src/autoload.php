481da450343f6ebb0d531e41b752672526279975
Ben <bhoogter@ford.com>
2020-01-17 16:59:37 -0500

    Improve autoloader

--- src/autoload.php
+++ src/autoload.php
@@ -1,3 +1,5 @@
 <?php
 
-require_once(__DIR__ . "/class-dependency-manager.php");
+spl_autoload_register(function ($name) {
+    if ($name == "dependency_manager") require_once(__DIR__ . "/class_dependency_manager.php");
+});
