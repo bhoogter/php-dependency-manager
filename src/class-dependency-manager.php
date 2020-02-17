b610c57e7be2bf26b36bca0735b0a2cf7b0585cf
Ben <bhoogter@ford.com>
2020-02-17 18:26:54 -0500

    Upgrade xml dep

--- src/class-dependency-manager.php
+++ src/class-dependency-manager.php
@@ -36,7 +36,7 @@ class dependency_manager
         // Should be maintained here, in this form, as well as dependencies.xml for propagation
         // Cannot use xml_file here, since it is the dependency we are pulling in....  Hence, internal.
         return array(
-            "github://bhoogter:xml-file/phar:0.2.64"
+            "github://bhoogter:xml-file/phar:0.2.65"
     );
     }
 
