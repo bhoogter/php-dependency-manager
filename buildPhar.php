dc056fe441f64e159ce4588cf7d140edb4669fd5
BHOOGTER <bhoogter@ford.com>
2022-01-05 14:09:10 -0500

    Save changes

--- buildPhar.php
+++ buildPhar.php
@@ -2,8 +2,6 @@
 
 unlink('php-dependency-manager.phar');
 $phar = new Phar('php-dependency-manager.phar');
-// $phar->buildFromDirectory('src');
-// $phar->buildFromIterator(new RecursiveIteratorIterator(new RecursiveDirectoryIterator('src')));
 $pattern = '#' . dirname(__FILE__) . '.src.*$#';
 $pattern = str_replace("\\", "\\\\", $pattern);
 $phar->buildFromDirectory(dirname(__FILE__), $pattern);
